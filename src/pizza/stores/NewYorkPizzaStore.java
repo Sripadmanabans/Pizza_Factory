220633f7e7e5e5c0284801d5a2bf8841e8ebb6d8
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-09-28 11:25:37 -0500

    Added New York Store.

---
+++ src/pizza/stores/NewYorkPizzaStore.java
@@ -0,0 +1,34 @@
+package pizza.stores;
+
+import base.Pizza;
+import base.PizzaStore;
+import pizza.types.newyork.NYStyleCheesePizza;
+import pizza.types.newyork.NYStylePepperoniPizza;
+import pizza.types.newyork.NYStyleVeggiePizza;
+
+/**
+ * This is the class that is used to serve the New York style pizza
+ * Created by Sripadmanaban on 9/28/2015.
+ */
+public class NewYorkPizzaStore extends PizzaStore {
+
+    @Override
+    public Pizza createPizza(String type) {
+        Pizza pizza;
+
+        switch (type) {
+            case "Cheese":
+                pizza = new NYStyleCheesePizza();
+                break;
+            case "Pepperoni":
+                pizza = new NYStylePepperoniPizza();
+                break;
+            default:
+                pizza = new NYStyleVeggiePizza();
+                break;
+        }
+
+        return pizza;
+    }
+
+}
