51cabf339b6058c6e936a4b3fc27c64b1c7da009
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-09-28 11:23:02 -0500

    Added Chicago store

---
+++ src/pizza/stores/ChicagoPizzaStore.java
@@ -0,0 +1,34 @@
+package pizza.stores;
+
+import base.Pizza;
+import base.PizzaStore;
+import pizza.types.chicago.ChicagoStyleCheesePizza;
+import pizza.types.chicago.ChicagoStylePepperoniPizza;
+import pizza.types.chicago.ChicagoStyleVeggiePizza;
+
+/**
+ * This is the class that is used to create the Chicago Pizzas.
+ * Created by Sripadmanaban on 9/28/2015.
+ */
+public class ChicagoPizzaStore extends PizzaStore {
+
+    @Override
+    public Pizza createPizza(String type) {
+        Pizza pizza;
+
+        switch (type) {
+            case "Cheese":
+                pizza = new ChicagoStyleCheesePizza();
+                break;
+            case "Pepperoni":
+                pizza = new ChicagoStylePepperoniPizza();
+                break;
+            default:
+                pizza = new ChicagoStyleVeggiePizza();
+                break;
+        }
+
+        return pizza;
+    }
+
+}
